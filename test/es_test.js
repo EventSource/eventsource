600a7a3fdcd451be6b5c98fd3d523cd443d8a404
Aslak Hellesoy <aslak.hellesoy@gmail.com>
2012-02-08 09:33:45 +0000

    More chunking tests

--- test/es_test.js
+++ test/es_test.js
@@ -2,11 +2,13 @@ var http = require('http');
 var EventSource = require('es');
 
 var port = 20000;
-function createServer(data, callback) {
+function createServer(chunks, callback) {
     var responses = [];
     var server = http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/event-stream'});
-        res.write(data);
+        chunks.forEach(function(chunk) {
+            res.write(chunk);
+        });
         responses.push(res);
     });
     function close() {
@@ -23,13 +25,40 @@ function createServer(data, callback) {
 }
 
 exports['Messages'] = {
-    'one-line simple message in one chunk': function(test) {
-        createServer("data: Hello\n\n", function(es, close) {
+    'one one-line message in one chunk': function(test) {
+        createServer(["data: Hello\n\n"], function(es, close) {
             es.onmessage = function(m) {
                 test.equal("Hello", m.data);
                 close();
                 test.done();
             };
+        });
+    },
+
+    'one one-line message in two chunks': function(test) {
+        createServer(["data: Hel", "lo\n\n"], function(es, close) {
+            es.onmessage = function(m) {
+                test.equal("Hello", m.data);
+                close();
+                test.done();
+            };
+        });
+    },
+
+    'two one-line messages in one chunk': function(test) {
+        createServer(["data: Hello\n\n", "data: World\n\n"], function(es, close) {
+            es.onmessage = first;
+
+            function first(m) {
+                test.equal("Hello", m.data);
+                es.onmessage = second;
+            }
+
+            function second(m) {
+                test.equal("World", m.data);
+                close();
+                test.done();
+            }
         });
     }
 };
