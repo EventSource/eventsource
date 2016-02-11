dedd8e55f27e52b5b3e93921a9ae2cb8ab87345a
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-02-11 14:18:25 +0100

    Emit `error` event when calling `EventSource.close()`

--- test/eventsource_test.js
+++ test/eventsource_test.js
@@ -462,6 +462,8 @@ describe('HTTP Request', function () {
 
         var es = new EventSource(server.url);
         es.onerror = function (err) {
+          if(this.readyState === EventSource.CLOSED) return
+
           assert.equal(err.status, status);
           server.close(done);
         };
@@ -588,6 +590,8 @@ describe('Reconnection', function () {
       es.reconnectInterval = 0;
 
       es.onerror = function (e) {
+        if(this.readyState === EventSource.CLOSED) return
+
         assert.equal(e.status, 204);
         server.close(function (err) {
           if(err) return done(err);
@@ -695,6 +699,8 @@ describe('readyState', function () {
     var es = new EventSource('http://localhost:' + _port);
     assert.equal(EventSource.CONNECTING, es.readyState);
     es.onerror = function () {
+      if(this.readyState === EventSource.CLOSED) return
+
       es.close();
       done();
     }
@@ -813,10 +819,12 @@ describe('Events', function () {
       es.addEventListener('open', function () {
         es.close();
         process.nextTick(function () {
-          server.close(done);
+          server.close();
         });
       });
       es.addEventListener('error', function () {
+        if(this.readyState === EventSource.CLOSED) return done()
+
         done(new Error('error should not be emitted'));
       });
     });
